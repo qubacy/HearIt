8bda94bc12be09990540f9afea2b4d1163c373f3
MCDead <souldzukaev@gmail.com>
2024-07-18 18:50:30 +0700

    Init. commit;

---
+++ app/src/main/java/com/qubacy/hearit/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.qubacy.hearit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.qubacy.hearit.ui.theme.HearItTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {
+            HearItTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/qubacy/hearit/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.qubacy.hearit.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
